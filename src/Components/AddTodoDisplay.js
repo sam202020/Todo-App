39676f554dd4c863e35903f92fd866f8ad6f72ea
Shalom <shalom@Shaloms-MBP.home>
2019-04-01 08:41:17 -0400

    add error checking for add and edit todo

--- src/Components/AddTodoDisplay.js
+++ src/Components/AddTodoDisplay.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useReducer } from "react";
+import { withStyles } from '@material-ui/core/styles';
 import { TextField, Paper, Button, Grid } from "@material-ui/core";
 
 import changeTodo from "./changeTodo";
 
+const initialState = { titleError: false, descError: false };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "error":
+      return action.error;
+    case "unError":
+      return initialState;
+    default:
+      throw new Error();
+  }
+}
+
+const styles = {
+  'input': {
+    '&::placeholder': {
+      color: 'red'
+    }
+  }
+};
+
 const AddtodoDisplay = props => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   const {
     _id,
     title,
@@ -12,26 +36,34 @@ const AddtodoDisplay = props => {
     onDescChange,
     changeTodo,
     type,
-    resetState
+    resetState,
+    errorCheck
   } = props;
+  let titleErrorStyle, descErrorStyle;
+  if (state.titleError) titleErrorStyle = { classes: {input: props.classes['input']} };
+  if (state.descError) descErrorStyle = { classes: {input: props.classes['input']} };
   return (
     <Paper style={{ margin: 16, padding: 16 }}>
       <Grid container>
         <Grid xs={10} md={11} item style={{ paddingRight: 16 }}>
           <TextField
+            error={state.titleError}
             placeholder={`${type} Todo Title Here`}
             value={title}
             onChange={onTitleChange}
             fullWidth
+            InputProps={titleErrorStyle}
           />
         </Grid>
 
         <Grid xs={10} md={11} item style={{ paddingRight: 16 }}>
           <TextField
+            error={state.descError}
             placeholder={`${type} Todo Description Here`}
             value={description}
             onChange={onDescChange}
             fullWidth
+            InputProps={descErrorStyle}
           />
         </Grid>
         <Grid xs={2} md={1} item>
@@ -40,9 +72,17 @@ const AddtodoDisplay = props => {
             color="secondary"
             variant="outlined"
             onClick={async () => {
+              let error = await errorCheck();
+              if (error) {
+                if (Object.values(error).includes(true)) {
+                  dispatch({ type: "error", error });
+                  return;
+                }
+              }
               let res = await changeTodo(_id, title, description);
               if (res.status === 200) {
                 resetState();
+                dispatch({ type: "unError" });
               }
             }}
           >
@@ -54,4 +94,4 @@ const AddtodoDisplay = props => {
   );
 };
 
-export default changeTodo(AddtodoDisplay);
+export default changeTodo(withStyles(styles)(AddtodoDisplay));
--- src/Components/changeTodo.js
+++ src/Components/changeTodo.js
@@ -1,28 +1,30 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
+import { ConsoleLogger } from "@aws-amplify/core";
 
-const changeTodo = (AddOrEdit) => props => {
+const changeTodo = AddOrEdit => props => {
   let { title, description } = props;
   let setTitle, setDesc;
   [title, setTitle] = useState(title || "");
   [description, setDesc] = useState(description || "");
+
+  const setState = e => setField =>
+    setField(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1));
 
   const resetState = () => {
     setTitle("");
     setDesc("");
   };
 
+  const checkTitleAndDesc = () => {
+    const [titleError, descError] = [!title, !description];
+    return {titleError, descError};
+  };
+
   return (
     <AddOrEdit
-      onTitleChange={e =>
-        setTitle(
-          e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1)
-        )
-      }
-      onDescChange={e =>
-        setDesc(
-          e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1)
-        )
-      }
+      onTitleChange={e => setState(e)(setTitle)}
+      onDescChange={e => setState(e)(setDesc)}
+      errorCheck={checkTitleAndDesc}
       resetState={resetState}
       {...props}
       title={title}
